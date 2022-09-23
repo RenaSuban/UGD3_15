76056d3a56931bd35c3bd2559ad22b5720c93edf
RenaSuban <irenesianrsuban@gmail.com>
2022-09-23 22:21:42 +0700

    manifest & splash screen update

--- app/src/main/java/com/unguided/ugd3_15/SplashScreen.kt
+++ app/src/main/java/com/unguided/ugd3_15/SplashScreen.kt
@@ -1,39 +1,9 @@
 package com.unguided.ugd3_15
 
-<<<<<<< HEAD
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.view.animation.AnimationUtils
-import android.widget.ImageView
-import java.util.jar.Manifest
-
-@Suppress("DEPRECATION")
-class SplashScreen : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-
-        val backgroundImage:ImageView = findViewById(R.id.logo_icon)
-        val sideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
-        backgroundImage.startAnimation(sideAnimation)
-
-
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 4000) // delaying for 4 seconds...
-
-
-
-=======
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-
 
 
 class SplashScreen : Activity() {
@@ -48,6 +18,5 @@ class SplashScreen : Activity() {
 
     companion object {
         private const val SPLASH_TIME_OUT = 3000
->>>>>>> origin/master
     }
 }
